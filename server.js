d2d1fdccb891ca0128b2d7a036bc38b84fde1c72
Ochanissi <mirelalex2094@gmail.com>
2019-03-05 17:36:58 +0200

    fixing a bug

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> {res.send('It is working!')})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`Server is running on port ${process.env.PORT}`);
 })
