b4dddaea602fbab0938eff372ee64eba760fc1cd
Ochanissi <mirelalex2094@gmail.com>
2019-03-06 18:09:30 +0200

    adding database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.end.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssd: true
   }
 });
