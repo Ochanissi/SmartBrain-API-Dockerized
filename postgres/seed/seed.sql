5e1ab93b2ea7c82255e1be115af507de1e0b8cb1
Ochanissi <mirelalex2094@gmail.com>
2019-07-04 10:45:30 +0300

    updating the profile

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -2,6 +2,6 @@ BEGIN TRANSACTION;
 
 INSERT INTO users (name, email, entries, joined) VALUES ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
 
-INSERT INTO login (hash, email) VALUES ('', 'jessie@gmail.com');
+INSERT INTO login (hash, email) VALUES ('$2y$12$g/kRwFUHBtoOMYu1iiHxVefYymA/7GXQA6lBZaAn8/RodCDcv6tE.', 'jessie@gmail.com');
 
 COMMIT;
--- server.js
+++ server.js
@@ -57,6 +57,7 @@ app.get('/', (req, res)=> {res.send('It is working!')})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
