229ab88814ff3a2beb0bab3ab136eb74117e5cf7
Ochanissi <mirelalex2094@gmail.com>
2019-07-07 20:28:09 +0300

    retieving auth token

--- controllers/signin.js
+++ controllers/signin.js
@@ -25,8 +25,14 @@ const handleSignin = (db, bcrypt, req, res) => {
       .catch(err => Promise.reject('Wrong credentials'))
   }
 
-  const getAuthTokenId = () => {
-    console.log('auth ok');
+  const getAuthTokenId = (req, req) => {
+    const { authorization } = req.headers;
+    return redisClient.get(authorization, (err, reply) => {
+      if (err || !reply) {
+        return res.status(400).json('Unauthorized');
+      }
+      return res.json({id: reply})
+    })
   }
 
   const signToken = (email) => {
@@ -53,7 +59,7 @@ const handleSignin = (db, bcrypt, req, res) => {
   const signinAuthentication = (db, bcrypt) => (req, res) => {
     const { authorization } = req.headers;
     return authorization ? 
-      getAuthTokenId() : 
+      getAuthTokenId(req, res) : 
       handleSignin(db, bcrypt, req, res)
         .then(data => {
           data.id && data.email ? createSessions(data) : Promise.reject(data)
